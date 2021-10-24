cda29fa833377cad656db1159d74f1e1296812ef
Andres Rodriguez <andresmikaelrodriguez@gmail.com>
2021-10-24 12:41:46 -0700

    Cleared home page

--- src/components/home_components/Home.jsx
+++ src/components/home_components/Home.jsx
@@ -8,32 +8,7 @@ const HomePage = () => {
 
     return (
         <>
-            <br></br><br></br>
-            <h1 style={{textAlign: center}}>Welcome, {userData.username}</h1>
-            <br></br><br></br><br></br>
-            <h2>Feed</h2>
-            <br></br>
-            <div>
-                <table>
-                    <tr>
-                        <th>Name</th>
-                        <th>Date</th>
-                        <th>Amount</th>
-                        <th>Description</th>
-                    </tr>
-                    <tr>
-                        <td>---</td>
-                        <td>---</td>
-                        <td>---</td>
-                        <td>---</td>
-                    </tr>
-                </table>
-            </div>
-            <br></br><br></br><br></br>
-            <div style={{textAlign: center}}>
-                <button type="submit" id="prev" onClick="#">Prev</button>
-                <button type="submit" id="next" onClick="#">Next</button>
-            </div>
+            <h1>Welcome, {userData.username}</h1>
         </>
     )
 }
